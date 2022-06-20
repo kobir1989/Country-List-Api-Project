110ee097018e13904dd77016890929ab948f30f9
kobir hossain Ritu <kobir.h.ritu@gmail.com>
2022-06-20 15:28:21 +0600

    UI modified

---
+++ UI.js
@@ -0,0 +1,18 @@
+const renderData = function (data) {
+  const html = `
+        <div class="nthChild data-row row">
+        <div class="col"><a class="link" href = "" target="_blank"> 
+          <img class= "flag-image" src="${
+            data.flags.png
+          }" alt = "flag.png"></a></div>
+        <div class="col">${data.name.common}</div>
+        <div class="col">${data.region}</div>
+        <div class="col">${data.capital}</div>
+        <div class="col">${(+data.population / 1000000).toFixed(
+          1
+        )} Million</div>
+           
+         </div>
+        `;
+  displayElement.insertAdjacentHTML("beforeend", html);
+};
---
+++ api.js
@@ -0,0 +1,32 @@
+const displayElement = document.querySelector(".API-data");
+const page2Element = document.querySelector(".country--details");
+const searchInput = document.querySelector("#search--input");
+
+////////////////////////////////////////////////////////////////
+//getting data and display in UI
+const getData = async function () {
+  const apiResponse = await fetch("https://restcountries.com/v3.1/all");
+  const response = await apiResponse.json();
+  const data = await response.forEach((data) => {
+    renderData(data);
+    console.log(data);
+  });
+};
+
+console.log(getData());
+
+// search bar
+
+searchInput.addEventListener("keydown", (e) => {
+  const userInput = e.target.value.toLowerCase();
+  document.querySelectorAll(".row").forEach((task) => {
+    const row = task.textContent;
+    console.log(row);
+    if (row.toLowerCase().indexOf(userInput) != -1) {
+      task.style.display = "";
+    } else {
+      task.style.display = "none";
+    }
+  });
+  console.log(userInput);
+});
